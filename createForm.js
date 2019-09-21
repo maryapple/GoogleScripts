9f03e579341baf06c66846766c17e048f7f40e8a
maryapple <maria9ku@gmail.com>
2019-09-21 18:39:38 +0300

    Collecting data

--- createForm.js
+++ createForm.js
@@ -1,17 +1,19 @@
-//var currentSheet = SpreadsheetApp.getActiveSpreadsheet().getActivesheet;
 var questionSheet = SpreadsheetApp.getActiveSpreadsheet().getSheetByName("Вопросы");
 
-function createForm(){
+function countQuestions() {
 	var range = questionSheet.getRange(2, 1, 30).getValues();
 	var quantityOfQuestions = 0;
 
 	for (var rowNumber = 2; questionSheet.getRange('A' + rowNumber).getValue() != ''; rowNumber++) {
 		quantityOfQuestions++;
 	}
+	return quantityOfQuestions;
+}
 
+function makeRandomNumbers() {
 	var arr = [];
 
-	for (var i = 0; i < 2; i++) {
+	for (var i = 0; i < 5; i++) {
 		var n = randomNum();
 		if (arr.indexOf(n) == -1) {
 			arr.push(n);
@@ -29,12 +31,37 @@ function createForm(){
 			}
 		}
 	}
-
-	for (var i = 0; i < 5; i++) {
-			
-	}
+	return arr;
 }
 
-function randomNum() { // Генерирует до 10
-	return Math.round(Math.random()*10);
+// Generates a number between 1 and 10
+function randomNum() {
+	return (Math.round(Math.random()*10) + 1);
+}
+
+function selectFieldsOfQuestion(index, amountOfAnswers) {
+	var question = questionSheet.getRange('B' + index).getValue();
+	var answers = [];
+	for (var i = 0; i < amountOfAnswers; i++) {
+		answers[i] = questionSheet.getRange(index, 7 + i).getValue();
+	}
+	var obj = {
+		question: question,
+		answers: answers
+	};
+	return obj;
+}
+
+function getDataForForm() {
+	var array = makeRandomNumbers();
+	var ind = 0;
+	var amountOfAnswers = 0;
+	var questionWithAnswers = {};
+
+	for (i in array) {
+		ind = array[i];
+		amountOfAnswers = questionSheet.getRange('F' + ind).getValue();
+		questionWithAnswers = selectFieldsOfQuestion(ind, amountOfAnswers);
+	}
+	return questionWithAnswers;
 }
